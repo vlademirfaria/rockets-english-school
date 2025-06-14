8c5974c32a5005014472963404da1c62c5fcf365
vlademirfaria <vemir38@hotmail.com>
2025-06-14 12:49:57 -0300

    sem chave do api

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 
 // IMPORTANTE: Cole a sua chave da API do Gemini aqui.
 // Este ficheiro NÃO deve ser enviado para o seu repositório Git.
-const GEMINI_API_KEY = "AIzaSyCsZfMOgKNywH6FUgVcPEXtgRGY9wTYaPc";
+const GEMINI_API_KEY = "";
